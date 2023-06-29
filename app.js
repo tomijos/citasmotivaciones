128140ea2be0b2135b3100cdbbe12261bd2e2c42
tomijos <w_contreras@outlook.com>
2023-06-29 10:43:27 -0600

    Citas Motivacionales

---
+++ app.js
@@ -0,0 +1,19 @@
+    //Selectores html
+    const boton = document.querySelector('#boton-cambiar-cita');
+    const cita = document.getElementById('cita');
+    const autor = document.getElementById('autor');
+    
+    //Generar un numero entero aleadotorio
+    // sin incluir max en los valores posibles
+    function numeroAleatorio(min, max){
+        return Math.floor(Math.random() * (max - min) + min);
+    } 
+    function cambiarCita(){        
+        let indiceAleatorio = numeroAleatorio(0, citas.length);
+        cita.innerText = `"${citas[indiceAleatorio].texto}"`;
+        autor.innerHTML = `${citas[indiceAleatorio].autor}`;  
+    }
+    
+    //Cambiar cita, al dar click al btn
+    boton.addEventListener('click',cambiarCita);
+    
---
+++ citas.js
@@ -0,0 +1,64 @@
+    //Selectores
+    let citas = [
+        
+            {
+                autor: "Vince Lombardi",
+                texto: "El éxito no es un accidente. Es trabajo duro, perseverancia, aprendizaje, estudio, sacrificio y, sobre todo, amor por lo que estás haciendo o aprendiendo a hacer."
+              },
+              {
+                autor: "Zig Ziglar",
+                texto: "El éxito no se logra solo con cualidades especiales. Es sobre todo un trabajo de constancia, de método y de organización."
+              },
+              {
+                autor: "Thomas Edison",
+                texto: "El genio es un uno por ciento de inspiración y un noventa y nueve por ciento de transpiración."
+              },
+              {
+                autor: "Napoleón Hill",
+                texto: "No hay limitaciones para la mente excepto las que uno se reconoce a sí mismo."
+              },
+              {
+                autor: "Michael Jordan",
+                texto: "He fallado una y otra vez en mi vida, por eso he tenido éxito."
+              },
+              {
+                autor: "Confucio",
+                texto: "Elige un trabajo que ames, y nunca tendrás que trabajar ni un solo día de tu vida."
+              },
+              {
+                autor: "Walt Disney",
+                texto: "La mejor manera de empezar algo es dejar de hablar de ello y empezar a hacerlo."
+              },
+              {
+                autor: "Albert Einstein",
+                texto: "La imaginación es más importante que el conocimiento. El conocimiento es limitado, mientras que la imaginación no."
+              },
+              {
+                autor: "Les Brown",
+                texto: "Demuéstrate a ti mismo que puedes hacer aquello que te propongas hacer."
+              },
+              {
+                autor: "Steve Jobs",
+                texto: "Tu tiempo es limitado, así que no lo desperdicies viviendo la vida de otra persona."
+              },              
+              { 
+                autor: "Steve Jobs", 
+                texto: "La programación es el arte de crear algo de la nada."
+               },
+               { 
+                autor: "Linus Torvalds", texto: "El software es como el sexo: es mejor cuando es gratis." 
+              },
+              { 
+                autor: "Bill Gates",
+                texto: "La programación te permite pensar en problemas y encontrar soluciones de manera creativa." 
+              },
+              { 
+                autor: "Grace Hopper",
+                texto: "La programación es una forma de liberar tu creatividad y hacer realidad tus ideas." 
+              },
+              { 
+                autor: "Larry Page",
+                 texto: "La programación te permite cambiar el mundo y dejar tu huella en él."
+             }
+        
+    ]
